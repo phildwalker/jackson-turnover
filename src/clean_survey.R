147b9ffbc3494d5b2ab2ee56d7d26410b7d2e2d5
philwalker <philip.walker@spacee.com>
2025-10-10 20:27:01 -0600

    first commit

---
+++ src/clean_survey.R
@@ -0,0 +1,6 @@
+# This will be a space to clean and perform some initial EDA of the survey data in order to use it 
+
+
+library(tidyverse)
+
+survey_response <- read_xlsx(here::here("data", "Senior HR Data & Insights Consultant - Work Sample - Engagement Data.xlsx.csv"))
---
+++ src/jolts_data.R
@@ -0,0 +1,6 @@
+# looking to capture the US labor statistics data (JOLTS: Job Openings and Labor Turnover Survey)
+# This will help us provide some context around how Jackson is performing relative to the US market as a whole
+
+
+library(devtools)
+install_github('mikeasilva/blsAPI')
